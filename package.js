45d86621cc0cdf145525dc689b9ef53752c454db
Ahad L. Amdani <ahad.amdani@gmail.com>
2015-04-17 23:15:34 -0500

    Minimum content for package.

---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  name: 'brwn:jquery-popup-overlay',
+  version: '1.0.0',
+  // Brief, one-line summary of the package.
+  summary: 'jquery-popup-overlay wrapper for Meteor.',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/aamdani/jquery-popup-overlay',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.on_use(function (api) {
+    api.versionsFrom('METEOR@1.1');
+    // dependecies required by package
+    //api.use('contributor:package-name@version.number.here','client')
+    // adding the required file for package
+    api.add_files('lib/jquery.popupoverlay.js', 'client');
+});
